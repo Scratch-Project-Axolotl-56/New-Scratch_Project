78c1da7661f619b54158890ea7469f39d677f0d3
Lisa Louison <lisa.louison@gmail.com>
2025-08-05 20:24:37 -0500

    fix(filename): rename files to js

---
+++ server/model/wordModel.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+dotenv.config();
+
+const PG_URI = process.env.WORD_URI;
+const pool = new Pool({ connectionString: PG_URI });
+
+export default {
+  query: (text, params, callback) => {
+    console.log('📋 executed query', text);
+    return pool.query(text, params, callback);
+  },
+};
