a20604ffb5b48197a55069d9eebdbc1c3fd48c83
Lisa Louison <lisa.louison@gmail.com>
2025-08-06 11:14:57 -0500

    begin keyboard functionality

--- client/src/components/game.tsx
+++ client/src/components/game.tsx
@@ -12,8 +12,29 @@ const App: React.FC = () => {
   const [guesses, setGuesses] = useState<string[]>([]);
   const [currentGuess, setCurrentGuess] = useState('');
   const [isGameOver, setIsGameOver] = useState(false);
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+  const letters = [
+    ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+    ['Enter', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Backspace'],
+  ];
 
   const wordLength = level + 3;
+  console.log('guessed letters', guessedLetters);
+  console.log('currentGuess', currentGuess);
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      console.log('Key pressed:', e);
+      //restrict pressing more keys than length of target word
+      handleKeyPress(e.key);
+    };
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
 
   useEffect(() => {
     // const wordList = wordsByLength[wordLength];
@@ -26,28 +47,63 @@ const App: React.FC = () => {
     setIsGameOver(false);
   }, [level, wordLength]);
 
-  const handleKey = (key: string) => {
-    if (isGameOver) return;
+  const handleKeyPress = (key: string) => {
+    console.log('key', key);
+    if (!isGameOver) {
+      if (key === 'Enter') {
+        handleEnter();
+      } else if (key === 'Backspace') {
+        handleBackspace();
+      } else if (/^[a-zA-Z]$/.test(key) && currentGuess.length < wordLength) {
+        handleAlphabetical(key);
+      } else {
+        //any other keys
+        return;
+      }
+    }
+    return;
+  };
 
-    if (key === 'Enter') {
-      if (currentGuess.length === wordLength) {
-        setGuesses([...guesses, currentGuess]);
-        if (currentGuess.toLowerCase() === targetWord) {
-          if (level < MAX_LEVEL) {
-            setTimeout(() => setLevel(level + 1), 1000);
-          } else {
-            setIsGameOver(true);
-          }
-        } else if (guesses.length >= 5 + level) {
+  const handleAlphabetical = (key: string) => {
+    console.log('in handle alphabetical');
+    // setCurrentGuess(currentGuess + key.toUpperCase());
+
+    setCurrentGuess((currentGuess) => {
+      const currentWordArray = currentGuess.split('');
+      console.log('currentWordArray', currentWordArray);
+      currentWordArray.push(key.toUpperCase());
+
+      const newWord = currentWordArray.join('');
+      console.log('newword', newWord, newWord.length);
+      return newWord;
+    });
+    setGuessedLetters((prevLetters) => [...prevLetters, key]);
+  };
+
+  const handleEnter = () => {
+    console.log(currentGuess, currentGuess.length, wordLength);
+    if (currentGuess.length === wordLength) {
+      setGuesses([...guesses, currentGuess]);
+      if (currentGuess.toLowerCase() === targetWord) {
+        if (level < MAX_LEVEL) {
+          setTimeout(() => setLevel(level + 1), 1000);
+        } else {
           setIsGameOver(true);
         }
-        setCurrentGuess('');
+      } else if (guesses.length >= 5 + level) {
+        setIsGameOver(true);
       }
-    } else if (key === 'Backspace') {
-      setCurrentGuess(currentGuess.slice(0, -1));
-    } else if (/^[a-zA-Z]$/.test(key) && currentGuess.length < wordLength) {
-      setCurrentGuess(currentGuess + key.toUpperCase());
+      setCurrentGuess('');
+    } else {
+      console.log('word length too short');
     }
+  };
+
+  const handleBackspace = () => {
+    if (currentGuess.length === 0) {
+      return;
+    }
+    setCurrentGuess(currentGuess.slice(0, -1));
   };
 
   return (
@@ -60,11 +116,11 @@ const App: React.FC = () => {
         wordLength={wordLength}
         targetWord={targetWord}
       />
-      <Keyboard onKeyPress={handleKey} />
+      <Keyboard onKeyDown={handleKeyPress} letters={letters} />
       {isGameOver && (
         <div>
           <p>
-            {currentGuess === targetWord ? '✅ LEVEL COMPLETE' : '💀 GAME OVER'}
+            {currentGuess === targetWord ? '✅ LEVEL COMPLETE' : '🚨 GAME OVER'}
           </p>
           <button onClick={() => setLevel(0)}>Restart</button>
         </div>
