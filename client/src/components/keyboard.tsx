a33a2a5893a29b344e6cc2dbfba211643f79e5fc
Lisa Louison <lisa.louison@gmail.com>
2025-08-07 11:00:21 -0500

    add missing type

--- client/src/components/keyboard.tsx
+++ client/src/components/keyboard.tsx
@@ -11,7 +11,7 @@ const Keyboard: React.FC<keyboardProps> = ({
   guesses,
   targetWord,
 }) => {
-  const letters = [
+  const letters: string[][] = [
     ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
     ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
     ['Enter', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Backspace'],
